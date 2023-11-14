12f680787d68b1c81ed41ef55fd5b8faf4b7f95f
obuzietter <obuyacalvince672@gmail.com>
2023-11-15 00:28:33 +0300

    trwaap

--- resources/views/achievements.blade.php
+++ resources/views/achievements.blade.php
@@ -6,11 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>High School Achievements</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="stylesheet" href="/css/achievements.css">
+    <!-- <link rel="stylesheet" href="/css/achievements.css"> -->
     <link rel="stylesheet" href="/css/navbar-footer.css">
     <!--ICONSCOUT CDN-->
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css">
-    <link rel="stylesheet" href="./css/style.css">
+    <!-- <link rel="stylesheet" href="./css/style.css"> -->
     <!--font-->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
